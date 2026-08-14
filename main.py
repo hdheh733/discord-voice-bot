2e4e9d9e6dc9cc96f83ee7a247aaec948f4bed9d
hdheh733 <hdheh733@outlook.com>
2026-08-14 03:35:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix='>', intents=intents)
+# إزالة أمر help المدمج لمنع التعارض
+bot = commands.Bot(command_prefix='>', intents=intents, help_command=None)
 
 YTDL_OPTIONS = {
     'format': 'bestaudio/best',
